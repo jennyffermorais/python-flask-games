a734859f9489506eb8395aca7d9b7c0d08b0b238
jennyffermorais <jennyffernovaes@hotmail.com>
2023-10-21 17:57:24 -0300

    implement email sending feature

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-import sqlite3
+# import sqlite3
 from flask import Flask, redirect, render_template, request
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 app = Flask(__name__)
+#app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Substitua pelo servidor SMTP do seu provedor de e-mail.
+app.config['MAIL_PORT'] = 465  # Porta do servidor SMTP (587 para TLS, 465 para SSL, 25 para não seguro).
+app.config['MAIL_USE_TLS'] = False  # Use TLS (True/False para SSL).
+app.config['MAIL_USE_SSL'] = True  # Use SSL (True/False para TLS).
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  # Seu endereço de e-mail.
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  # Sua senha de e-mail.
 
+mail = Mail(app)
 # Conectar-se ao banco de dados (ou criar um se não existir)
-conn = sqlite3.connect('games.db', check_same_thread=False)
+# conn = sqlite3.connect('games.db', check_same_thread=False)
 
 # Criar um cursor
 # Um cursor é um objeto que permite que você execute comandos SQL no banco de dados
-cursor = conn.cursor()
+# cursor = conn.cursor()
 
 # Criar uma tabela
-cursor.execute('''CREATE TABLE IF NOT EXISTS registrants (id INTEGER, name TEXT NOT NULL, sport TEXT NOT NULL, PRIMARY KEY(id))''')
+# cursor.execute('''CREATE TABLE IF NOT EXISTS registrants (id INTEGER, name TEXT NOT NULL, sport TEXT NOT NULL, PRIMARY KEY(id))''')
 
 # REGISTRANTS = {}
 
@@ -29,10 +43,10 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    name = request.form.get("name")
+    email = request.form.get("email")
 
-    if not name:
-        return render_template("error.html", message="Missing name")
+    if not email:
+        return render_template("error.html", message="Missing email")
     sport = request.form.get("sport")
     if not sport:
         return render_template("error.html", message="Missing sport")
@@ -42,13 +56,17 @@ def register():
     #REGISTRANTS[name] = sport
 
     # Inserir dados
-    cursor.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", (name, sport))
+    # cursor.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", (name, sport))
 
-    return redirect("/registrants")
+    message = Message("You are registered!", sender=os.getenv('MAIL_USERNAME'), recipients=[email])
+    mail.send(message)
 
+    return render_template("success.html")
+"""
 @app.route("/registrants")
 def registrants():
     # Consultar dados
-    registrants = cursor.execute("SELECT * FROM registrants").fetchall()
+    # registrants = cursor.execute("SELECT * FROM registrants").fetchall()
     print(registrants)
     return render_template("registrants.html", registrants=registrants) # variável do template e variável do select
+"""
